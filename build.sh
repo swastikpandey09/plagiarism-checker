3fb2a399a0e4dd255e5f799171a55267843e1a13
swastikpandey09 <160395929+swastikpandey09@users.noreply.github.com>
2025-07-14 13:47:34 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 pip install -r requirements.txt
+uvicorn main:app --host 0.0.0.0 --port 10000 --reload
